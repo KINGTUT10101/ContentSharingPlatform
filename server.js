1b8ca2f1ab4ff3291d260de384e19af6961c9879
KINGTUT10101 <45105509+KINGTUT10101@users.noreply.github.com>
2023-11-01 15:33:45 -0500

    Update server to use cors package

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
-dotenv.config({ path: './config/config.env' });
+const PORT = process.env.PORT || 5000;
+
+let corsOptions = {
+  origin: `http://localhost:{PORT}`
+};
+
+app.use(cors(corsOptions))
+app.use(express.urlencoded({ extended: true }))
+dotenv.config({ path: './config/config.env' })
 
 const app = express();
 
 app.get('/', (req, res) => res.send('Server running'));
 
-const PORT = process.env.PORT || 5000;
+
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
