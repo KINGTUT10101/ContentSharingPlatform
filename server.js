b9298fb16687018f53a9f41ce7cf3d73d019fc36
KINGTUT10101 <45105509+KINGTUT10101@users.noreply.github.com>
2023-11-06 13:38:59 -0600

    Fix URL navigation breaking the server

--- server.js
+++ server.js
@@ -2,35 +2,32 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
 const PORT = process.env.PORT || 5000;
-const app = express()
+const app = express();
 let corsOptions = {
-  origin: `http://localhost:{PORT}`
+  origin: `http://localhost:${PORT}`
 };
 
-app.use(cors(corsOptions))
-app.use(express.urlencoded({ extended: true }))
-dotenv.config({ path: './config/config.env' })
-app.use(express.static('client/build'));
+// Replace __dirname with a similar variable using the new ES Modules syntax
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+app.use(cors(corsOptions));
+app.use(express.urlencoded({ extended: true }));
+dotenv.config({ path: './config/config.env' });
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 // API Functions should look like this
-app.get('/test', (req, res) => {
-  res.send('Server running')
+app.get('/api/test', (req, res) => {
+  res.send('Server running');
 });
 
 // Serves the static frontend content
-// Make sure the API paths don't conflict with the React Router paths in client/src/App.js
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 
-// Error handler
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
+app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
-
-
-
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
