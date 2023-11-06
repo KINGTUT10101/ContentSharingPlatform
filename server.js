1fe796b810c0d170f7b8f207337ba1dc65a671f7
KINGTUT10101 <45105509+KINGTUT10101@users.noreply.github.com>
2023-11-06 09:58:52 -0600

    Make backend serve static frontend content

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import path from 'path';
 
 const PORT = process.env.PORT || 5000;
 const app = express()
@@ -11,8 +12,18 @@ let corsOptions = {
 app.use(cors(corsOptions))
 app.use(express.urlencoded({ extended: true }))
 dotenv.config({ path: './config/config.env' })
+app.use(express.static('client/build'));
 
-app.get('/', (req, res) => res.send('Server running'));
+// API Functions go here
+app.get('/test', (req, res) => {
+  res.send('Server running')
+});
+
+// Serves the static frontend content
+// Make sure the API paths don't conflict with the React Router paths in client/src/App.js
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 
 
