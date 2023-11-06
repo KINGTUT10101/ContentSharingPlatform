821875f538dc6cf494ff8a14218324447c1aacc0
KINGTUT10101 <45105509+KINGTUT10101@users.noreply.github.com>
2023-11-06 13:39:40 -0600

    Clean up not found component

--- client/src/pages/NotFound.js
+++ client/src/pages/NotFound.js
@@ -1,15 +1,10 @@
-import { useParams } from 'react-router-dom';
-import { Typography, Grid, Pagination } from '@mui/material';
-import ProfileCard from "../components/ProfileCard"
-import ContentCard from "../components/ContentCard"
+import { Typography } from '@mui/material';
 
 /**
  * Shown when the user navigates to an invalid URL on the site
  * @returns {JSX.Element} A NotFound component.
  */
 export default function NotFound() {
-  let { param1 } = useParams();
-
   return (
     <div>
       <Typography align="center" variant="h3">
