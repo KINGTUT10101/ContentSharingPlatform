3eacf83db172f09ed8a8ff3979793ee0cba3c133
KINGTUT10101 <45105509+KINGTUT10101@users.noreply.github.com>
2023-11-20 13:07:00 -0600

    Fixed and improved SQL table script

--- database/PostgreSQL_Project.sql
+++ database/PostgreSQL_Project.sql
@@ -1,12 +1,12 @@
 -- PostgreSQL Table for UserAccount
 CREATE TABLE UserAccount (
-    Email VARCHAR(255) PRIMARY KEY,
-    Username VARCHAR(255) UNIQUE NOT NULL,
+    Email VARCHAR(255) PRIMARY KEY CHECK (Email NOT LIKE '% %'),
+    Username VARCHAR(255) UNIQUE NOT NULL CHECK (Username NOT LIKE '% %'),
     Password TEXT NOT NULL,
     AccountType VARCHAR(50) CHECK (AccountType IN ('standard', 'admin')),
     CreationDate TIMESTAMP NOT NULL,
     AccountStatus VARCHAR(50) CHECK (AccountStatus IN ('active', 'restricted', 'banned')),
-    ProfilePic VARCHAR(255);
+    ProfilePic VARCHAR(255),
     Bio TEXT
 );
 -- PostgreSQL Table for FeaturedContent
@@ -41,17 +41,9 @@ CREATE TABLE Rating (
     PRIMARY KEY (UserEmail, ContentID)
 );
 CREATE VIEW averageRating AS	
-	SELECT contentid, avg(CAST(ratingtype AS INT))
-	FROM rating
-	GROUP BY contentid
-
-ALTER TABLE UserAccount
-ADD CONSTRAINT username_no_space_check 
-CHECK (Username NOT LIKE '% %');
-
-ALTER TABLE UserAccount
-ADD CONSTRAINT email_no_space_check 
-CHECK (Email NOT LIKE '% %');
+    SELECT contentid, avg(CAST(ratingtype AS INT))
+    FROM rating
+    GROUP BY contentid;
 
 -- Indexes for the Comment Table
 CREATE INDEX idx_comment_useremail ON Comment(UserEmail);
@@ -61,25 +53,3 @@ CREATE INDEX idx_comment_contentid ON Comment(ContentID);
 CREATE INDEX idx_report_itemid ON Report(ItemID);
 CREATE INDEX idx_report_reporterid ON Report(ReporterID);
 CREATE INDEX idx_report_reportstatus ON Report(ReportStatus);
-/*
--- in order
-INSERT INTO UserAccount (Email, Username, Password, AccountType, CreationDate, AccountStatus, Bio) VALUES
-('john.doe@example.com', 'JohnDoe', 'hashed_password1', 'standard', '2023-04-01 10:00:00', 'active', 'Just a regular John Doe.'),
-('jane.smith@example.com', 'JaneSmith', 'hashed_password2', 'admin', '2023-04-02 11:00:00', 'restricted', 'Jane Smith here, love photography and tech.');
-
-INSERT INTO FeaturedContent (ContentID, FeaturedDate) VALUES
-(1, '2023-04-03 09:00:00'),
-(2, '2023-04-04 09:30:00');
-
-INSERT INTO Comment (UserEmail, ContentID, CommentText, CreationDate) VALUES
-('john.doe@example.com', 1, 'This is an interesting piece of content!', '2023-04-03 10:00:00'),
-('jane.smith@example.com', 1, 'Really enjoyed this, thanks for sharing!', '2023-04-03 10:15:00');
-
-INSERT INTO Report (ItemID, ReporterID, ItemType, ReportText, CreationDate, ReportType, ReportStatus) VALUES
-(1, 'john.doe@example.com', 'comment', 'This comment is inappropriate.', '2023-04-04 10:00:00', 'Abuse', 'Open'),
-(2, 'jane.smith@example.com', 'content', 'This content is stolen from another creator.', '2023-04-04 10:30:00', 'Copyright', 'Reviewed');
-
-INSERT INTO Rating (UserEmail, ContentID, RatingType) VALUES
-('john.doe@example.com', 1, 'positive'),
-('jane.smith@example.com', 1, 'negative');
-*/
