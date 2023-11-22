f5eb4c0b3cf651ade6de8b996284b1a9cfdadb2a
KINGTUT10101 <45105509+KINGTUT10101@users.noreply.github.com>
2023-11-22 09:57:10 -0600

    Add content upload and browse functionality

--- server/qrs/queries.js
+++ server/qrs/queries.js
@@ -2,6 +2,11 @@ import express from "express";
 import sbd from "../db/sqlConn.js";
 import mdb from "../db/mongoConn.js";
 import { ObjectId } from "mongodb";
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const router = express.Router();
 
@@ -11,56 +16,131 @@ process.on('uncaughtException', function (err) {
 
 //MONGO
     //CONTENT
-async function getContent (ContentID, proj){
-  let collection = mdb.collection('Content');
-  let query = {'_id': new ObjectId(ContentID)};
-  let result = await collection.findOne(query, proj);
-  if (!result) return -1;
-  const AR = await sbd.query(`SELECT avg, Username
-                              FROM averageRating, (SELECT username
-					                                         FROM UserAccount
-					                                         WHERE email = $1)
-                              WHERE contentid = $2`, [result.AuthorEmail, ContentID]);
-  if (!AR) {return -1;}
-  result.avgRat = AR.rows[0].avg;
-  result.username = AR.rows[0].username;
-  delete result.AuthorEmail;
-  return result;
-};
+  async function getContent (ContentID, proj){
+    let collection = mdb.collection('Content');
 
-//api/contentDetails/:ContentID
-router.get('/contentDetails/:ContentID', async (req, res) => { 
-  let proj = {projection: {_id:0, ContentType:0, ContentFileSize:0, CreationDate:0}}
-  const result = await getContent(req.params.ContentID,proj);
-  if(!result) res.status(404).send('Not found');
-  res.status(200).send(result);
+    // Content content data from mongodb
+    let query = {'_id': new ObjectId(ContentID)};
+    let contentResult = await collection.findOne(query, proj);
+    if (!contentResult) return -1;
+
+    // Get average rating
+    const avgRatingResult = await sbd.query(`SELECT avg
+                                              FROM averagerating
+                                              WHERE contentid = $1`, [ContentID]);
+    if (avgRatingResult.rows.length === 0) {
+      contentResult.avgRat = 0.5
+    }
+    else {
+      contentResult.avgRat = avgRatingResult.rows[0].avg
+    }
+
+    // Get username
+    const usernameResult = await sbd.query(`SELECT username
+                                              FROM useraccount
+                                              WHERE email = $1`, [contentResult.AuthorEmail]);
+    if (usernameResult.rows.length === 0) {return -1;}
+    contentResult.username = usernameResult.rows[0].username;
+    delete contentResult.AuthorEmail;
+
+    return contentResult;
+  };
+
+  //api/contentDetails/:ContentID
+  router.get('/contentDetails/:ContentID', async (req, res) => { 
+    let proj = {projection: {_id:0, ContentType:0, ContentFileSize:0, CreationDate:0}}
+    const result = await getContent(req.params.ContentID,proj);
+    if(!result) res.status(404).send('Not found');
+    res.status(200).send(result);
+  });
+
+  //api/contentCard/:ContentID
+  router.get('/contentCard/:ContentID', async (req, res) => {
+    let proj = {projection: {_id:0, ContentType:0, ContentFileSize:0, CreationDate:0, Description:0}}
+    const result = await getContent(req.params.ContentID,proj);
+    if(!result) res.status(404).send('Not found');
+    else res.status(200).send(result);
+  });
+
+  // Uploads a piece of content to localstorage and the database
+  router.post('/contentUpload', async (req, res) => {  
+    //get JSON doc
+    let doc = req.body;
+    // Add remaining data
+    doc.CreationDate = new Date()
+    doc.UpdatedDate = doc.CreationDate;
+    doc.Downloads = 0;
+    const fileData = doc.FileData;
+    delete doc.FileData;
+
+    // Upload document
+    let collection = mdb.collection('Content');
+    let result = await collection.insertOne(doc);
+    if(!result) {
+      res.status(404).send('Upload Error');
+      return
+    }
+    
+    // Upload filedata
+    const filePath = path.join(__dirname, "..", 'media', 'mapData', `${result.insertedId}.slf`)
+    const fileBuffer = Buffer.from(fileData, 'base64')
+    fs.writeFile(filePath, fileBuffer, (err)=>{
+      if (err) {
+        console.error("Error saving file:", err)
+        res.status(500).json('Error saving file')
+        collection.deleteOne ({"_id": result.insertedId})
+      }
+    });
+    res.status(200).send(result.insertedId);
+  });
+
+  //api/browseContent?page={page}&count={count}&sortBy={sortBy}&tags={tags}&searchString={searchString}
+  router.get('/browseContent', async (req, res) => {
+    const { page = 1, count = 20, sortBy = 'Downloads', tags, searchString } = req.query;
+
+    let offset = count * (page - 1);
+    if (offset < 0) {
+        res.status(400).send('Invalid query parameters');
+        return;
+    }
+
+    // Add sorting logic
+    let contentQuery = {};
+    let sortCriteria = {}
+    if (sortBy === 'Downloads' || sortBy === 'UpdatedDate') {
+      sortCriteria[sortBy] = -1; // 1 for ascending order, -1 for descending
+    } else {
+      sortCriteria.Downloads = 1
+    }
+
+    // Add tags filter
+    if (tags) {
+        const tagArray = tags.split(',');
+        contentQuery.tags = { $in: tagArray };
+    }
+
+    // Add search string filter
+    if (searchString && searchString.trim() !== '') {
+      contentQuery.$text = { $search: searchString.replace(/\+/g, ' ') };
+    }
+
+    try {
+        const collection = mdb.collection('Content');
+        const documents = await collection.find(contentQuery)
+            .sort(sortCriteria)
+            .skip(offset)
+            .limit(parseInt(count))
+            .project({ _id: 1 })
+            .toArray();
+
+        const contentIDs = documents.map(doc => doc._id);
+        res.json(contentIDs)
+    } catch (error) {
+        console.error('Error fetching content IDs:', error);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
-//api/contentCard/:ContentID
-router.get('/contentCard/:ContentID', async (req, res) => {
-  let proj = {projection: {_id:0, ContentType:0, ContentFileSize:0, CreationDate:0, Description:0}}
-  const result = await getContent(req.params.ContentID,proj);
-  if(!result) res.status(404).send('Not found');
-  else res.status(200).send(result);
-});
-
-// Gets the number of pieces of content that the user has uploaded
-router.get('/contentCount/:Username', async (req, res) => {
-  const username = req.params.Username
-  // Gets the email for the user
-  const sqlResult = await sbd.query(`SELECT Email
-                                  FROM UserAccount 
-                                  WHERE Username = $1
-                                  LIMIT 1`, [username]);
-  if (sqlResult.rows.length === 0) res.status(404).send('Not found');
-  const email = sqlResult.rows[0].email
-
-  // Count the number of documents for the email in MongoDB
-  let collection = mdb.collection('Content');
-  const query = {AuthorEmail: email}
-  let mdbResult = await collection.countDocuments(query);
-  res.status(200).send({count: mdbResult});
-});
 
 //SQL
     //USER
@@ -90,8 +170,7 @@ router.get('/contentCount/:Username', async (req, res) => {
                                     WHERE contentid = $1
                                     ORDER BY c.CreationDate DESC
                                     LIMIT $2 OFFSET $3`,[req.params.ContentID,limit,offset]);
-    if (result.rows.length === 0) res.status(404).send('Not Found');
-    else res.status(200).send(result.rows);
+    res.status(200).send(result.rows);
   });
 
   //api/commentCount/{ContentID}
